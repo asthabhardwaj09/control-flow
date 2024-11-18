b7219ec64a059e540cc5c57f7b6a4d397205b447
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2024-11-18 13:49:22 +0530

    control flow

---
+++ control_flow.js
@@ -0,0 +1,58 @@
+// if statement
+
+// if (2==="2") {
+//     console.log("executed");   
+// }
+// //<, >, <=, >=, ==, !=, ===
+
+// const temperature=41
+
+// if (temperature===20) {
+//     console.log("temperature is less than 41");
+// }
+// else{
+//     console.log("temperature is greater than 41");
+// }
+
+// console.log("Execute");
+
+// const score=200
+
+// if (score>100) {
+//     let power= "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+
+// const balance=1000
+// // if (balance>500) console.log("test"), console.log("test2");
+
+// if (balance<500) {
+//     console.log("less than");
+// }
+// else if(balance<750){
+//     console.log("less than 750");
+// }
+// else if(balance<900){
+//     console.log("less than 900");
+// }
+// else{
+//     console.log("less than 1200");
+// }
+
+const userloggedin=true
+const userdeebitcard=true
+const loogedinfromgoggle=false
+const loggedinfromemail=true
+
+if(userloggedin && userdeebitcard && 2==3){
+    console.log("allow to buy course");
+    
+}
+
+if(loggedinfromemail || loogedinfromgoggle || 2==22){
+    console.log("user logged in"); 
+}
+
+
